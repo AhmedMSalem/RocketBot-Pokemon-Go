7ab265a3e750d376ecc4dc5905352baa79da6028
Furtif <msntchat@hotmail,fr>
2017-02-21 23:15:43 +0100

    Osx-rev6

---
+++ PoGo.NecroBot.Logic/Model/Settings/EvolveFilter.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using POGOProtos.Enums;
+using System;
+
+namespace PoGo.NecroBot.Logic.Model.Settings
+{
+    [JsonObject(Description = "", ItemRequired = Required.DisallowNull)] //Dont set Title
+    public class EvolveFilter
+    {
+        public EvolveFilter()
+        {
+            Moves = new List<List<PokemonMove>>();
+            EnableEvolve = true;
+            Operator = "or";
+        }
+
+
+        public EvolveFilter(int evolveIV, int evolveLV, int minCP, string evoOperator = "and", string evolveTo = "", List<List<PokemonMove>> moves = null)
+        {
+            this.Moves = new List<List<PokemonMove>>();
+            if (moves != null) this.Moves = moves;
+            EnableEvolve = true;
+            this.MinIV = evolveIV;
+            this.MinLV = evolveLV;
+            this.EvolveTo = evolveTo;
+            this.MinCP = minCP;
+            this.Operator = evoOperator;
+        }
+
+        [NecrobotConfig(IsPrimaryKey = true, Key = "Enable Envolve", Description = "Allow bot auto evolve this pokemon", Position = 1)]
+        [DefaultValue(false)]
+        [JsonIgnore]
+        public bool EnableEvolve { get; set; }
+
+        [NecrobotConfig(Key = "Evolve Min IV", Description = "Min IV for auto evolve", Position = 2)]
+        [DefaultValue(95)]
+        [Range(0, 100)]
+        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 1)]
+        public int MinIV { get; set; }
+
+        [NecrobotConfig(Key = "Evolve Min LV", Description = "Min LV for auto evolve", Position = 3)]
+        [DefaultValue(95)]
+        [Range(0, 999)]
+        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 1)]
+        public int MinLV { get; set; }
+
+        [NecrobotConfig(Key = "Evolve Min CP", Description = "Min CP for auto evolve", Position = 4)]
+        [DefaultValue(10)]
+        [Range(0, 9999)]
+        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 1)]
+        public int MinCP { get; set; }
+
+        [NecrobotConfig(Key = "Moves", Description = "Define list of desire move for evolve", Position = 5)]
+        [DefaultValue(null)]
+        [JsonProperty(Required = Required.Default, DefaultValueHandling = DefaultValueHandling.Populate, Order = 2)]
+        public List<List<PokemonMove>> Moves { get; set; }
+
+        [NecrobotConfig(Key = "Operator", Position = 6, Description = "The operator logic use to check for evolve")]
+        [DefaultValue("or")]
+        [EnumDataType(typeof(Operator))]
+        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 5)]
+        public string Operator { get; set; }
+
+        [NecrobotConfig(Key = "Evolve To", Position = 6, Description = "Select branch to envolve to for multiple branch pokemon like Poliwirl")]
+        [DefaultValue("")]
+        [JsonProperty(Required = Required.DisallowNull, DefaultValueHandling = DefaultValueHandling.Populate, Order = 6)]
+        public string EvolveTo { get; set; }
+
+        [JsonIgnore]
+        public PokemonId EvolveToPokemonId
+        {
+            get
+            {
+                PokemonId id = PokemonId.Missingno;
+
+                if (Enum.TryParse<PokemonId>(this.EvolveTo, out id))
+                {
+                    return id;
+                }
+
+                return id;
+            }
+        }
+        internal static Dictionary<PokemonId, EvolveFilter> Default()
+        {
+            return new Dictionary<PokemonId, EvolveFilter>
+            {
+                {PokemonId.Rattata, new EvolveFilter(0, 0, 0, "or")},
+                {PokemonId.Zubat, new EvolveFilter(0, 0, 0, "or")},
+                {PokemonId.Pidgey, new EvolveFilter(0, 0, 0, "or")},
+                {PokemonId.Caterpie, new EvolveFilter(0, 0, 0,  "or") },
+                {PokemonId.Weedle, new EvolveFilter(0, 0, 0,  "or") },
+
+                {PokemonId.Porygon, new EvolveFilter(100, 28, 500, "and",PokemonId.Porygon2.ToString())},
+                {PokemonId.Gloom , new EvolveFilter(100, 28, 500, "and",PokemonId.Bellossom.ToString())} ,
+                {PokemonId.Sunkern , new EvolveFilter(100, 28, 500, "and",PokemonId.Sunflora.ToString())}  ,
+                {PokemonId.Slowpoke, new EvolveFilter(100, 28, 500, "and",PokemonId.Slowking.ToString())},
+                {PokemonId.Poliwhirl , new EvolveFilter(100, 28, 500, "and",PokemonId.Politoed.ToString())},
+                {PokemonId.Seadra , new EvolveFilter(100, 28, 500, "and",PokemonId.Kingdra.ToString())},
+                {PokemonId.Dratini, new EvolveFilter(100, 30, 800, "and")}
+
+            };
+        }
+    }
+}
---
+++ PoGo.NecroBot.Logic/Navigation.cs
@@ -0,0 +1,287 @@
+﻿#region using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using PoGo.NecroBot.Logic.Event;
+using PoGo.NecroBot.Logic.Interfaces.Configuration;
+using PoGo.NecroBot.Logic.Model;
+using PoGo.NecroBot.Logic.State;
+using PoGo.NecroBot.Logic.Strategies.Walk;
+using PokemonGo.RocketAPI;
+using POGOProtos.Networking.Responses;
+using GeoCoordinatePortable;
+using POGOProtos.Map.Fort;
+using PoGo.NecroBot.Logic.Utils;
+using PokemonGo.RocketAPI.Extensions;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+
+#endregion
+
+namespace PoGo.NecroBot.Logic
+{
+    public delegate void UpdatePositionDelegate(ISession session, double lat, double lng, double speed);
+
+    //add delegate
+    public delegate void GetHumanizeRouteDelegate(List<GeoCoordinate> route);
+
+    public class Navigation
+    {
+        public IWalkStrategy WalkStrategy { get; set; }
+        private readonly Client _client;
+        private Random WalkingRandom = new Random();
+        private List<IWalkStrategy> WalkStrategyQueue { get; set; }
+        public Dictionary<Type, DateTime> WalkStrategyBlackList = new Dictionary<Type, DateTime>();
+
+        public Navigation(Client client, ILogicSettings logicSettings)
+        {
+            _client = client;
+
+            InitializeWalkStrategies(logicSettings);
+            WalkStrategy = GetStrategy(logicSettings);
+        }
+
+        public double VariantRandom(ISession session, double currentSpeed)
+        {
+            if (WalkingRandom.Next(1, 10) > 5)
+            {
+                if (WalkingRandom.Next(1, 10) > 5)
+                {
+                    var randomicSpeed = currentSpeed;
+                    var max = session.LogicSettings.WalkingSpeedInKilometerPerHour +
+                              session.LogicSettings.WalkingSpeedVariant;
+                    randomicSpeed += WalkingRandom.NextDouble() * (0.02 - 0.001) + 0.001;
+
+                    if (randomicSpeed > max)
+                        randomicSpeed = max;
+
+                    if (Math.Round(randomicSpeed, 2) != Math.Round(currentSpeed, 2))
+                    {
+                        session.EventDispatcher.Send(new HumanWalkingEvent
+                        {
+                            OldWalkingSpeed = currentSpeed,
+                            CurrentWalkingSpeed = randomicSpeed
+                        });
+                    }
+
+                    return randomicSpeed;
+                }
+                else
+                {
+                    var randomicSpeed = currentSpeed;
+                    var min = session.LogicSettings.WalkingSpeedInKilometerPerHour -
+                              session.LogicSettings.WalkingSpeedVariant;
+                    randomicSpeed -= WalkingRandom.NextDouble() * (0.02 - 0.001) + 0.001;
+
+                    if (randomicSpeed < min)
+                        randomicSpeed = min;
+
+                    if (Math.Round(randomicSpeed, 2) != Math.Round(currentSpeed, 2))
+                    {
+                        session.EventDispatcher.Send(new HumanWalkingEvent
+                        {
+                            OldWalkingSpeed = currentSpeed,
+                            CurrentWalkingSpeed = randomicSpeed
+                        });
+                    }
+
+                    return randomicSpeed;
+                }
+            }
+
+            return currentSpeed;
+        }
+
+        private object ensureOneWalkEvent = new object();
+
+        public async Task Move(IGeoLocation targetLocation,
+            Func<Task> functionExecutedWhileWalking,
+            ISession session,
+            CancellationToken cancellationToken, double customWalkingSpeed = 0.0)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            TinyIoC.TinyIoCContainer.Current.Resolve<MultiAccountManager>().ThrowIfSwitchAccountRequested();
+
+            //add routes to map
+            var points = new List<GeoCoordinate>();
+            var route = Route(session,
+                new GeoCoordinate(
+                    _client.CurrentLatitude,
+                    _client.CurrentLongitude,
+                    _client.CurrentAltitude),
+                targetLocation.ToGeoCoordinate());
+
+            foreach (var item in route)
+                points.Add(new GeoCoordinate(item.ToArray()[1], item.ToArray()[0]));
+
+            //get pokeStops to map
+            OnGetHumanizeRouteEvent(points);
+            //end code add routes
+
+            // If the stretegies become bigger, create a factory for easy management
+
+            //Logging.Logger.Write($"Navigation - Walking speed {customWalkingSpeed}");
+            await WalkStrategy.Walk(targetLocation, functionExecutedWhileWalking, session, cancellationToken, customWalkingSpeed);
+        }
+
+        private void InitializeWalkStrategies(ILogicSettings logicSettings)
+        {
+            WalkStrategyQueue = new List<IWalkStrategy>();
+
+            // Maybe change configuration for a Navigation Type.
+            if (logicSettings.DisableHumanWalking)
+            {
+                WalkStrategyQueue.Add(new FlyStrategy(_client));
+            }
+
+            if (logicSettings.UseGpxPathing)
+            {
+                WalkStrategyQueue.Add(new HumanPathWalkingStrategy(_client));
+            }
+
+            if (logicSettings.UseGoogleWalk)
+            {
+                WalkStrategyQueue.Add(new GoogleStrategy(_client));
+            }
+
+            if (logicSettings.UseMapzenWalk)
+            {
+                WalkStrategyQueue.Add(new MapzenNavigationStrategy(_client));
+            }
+
+            if (logicSettings.UseYoursWalk)
+            {
+                WalkStrategyQueue.Add(new YoursNavigationStrategy(_client));
+            }
+
+            WalkStrategyQueue.Add(new HumanStrategy(_client));
+        }
+
+        public bool IsWalkingStrategyBlacklisted(Type strategy)
+        {
+            if (!WalkStrategyBlackList.ContainsKey(strategy))
+                return false;
+
+            DateTime now = DateTime.Now;
+            DateTime blacklistExpiresAt = WalkStrategyBlackList[strategy];
+            if (blacklistExpiresAt < now)
+            {
+                // Blacklist expired
+                WalkStrategyBlackList.Remove(strategy);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void BlacklistStrategy(Type strategy)
+        {
+            // Black list for 1 hour.
+            WalkStrategyBlackList[strategy] = DateTime.Now.AddHours(1);
+        }
+
+        public IWalkStrategy GetStrategy(ILogicSettings logicSettings)
+        {
+            return WalkStrategyQueue.First(q => !IsWalkingStrategyBlacklisted(q.GetType()));
+        }
+
+        //functions for routes map
+        private List<List<double>> Route(ISession session, GeoCoordinate start, GeoCoordinate dest)
+        {
+            var result = new List<List<double>>();
+
+            try
+            {
+                var web = WebRequest.Create(
+                    $"https://maps.googleapis.com/maps/api/directions/json?origin={start.Latitude},{start.Longitude}&destination={dest.Latitude},{dest.Longitude}&mode=walking&units=metric&key={session.LogicSettings.GoogleApiKey}");
+                web.Credentials = CredentialCache.DefaultCredentials;
+
+                string strResponse;
+                using (var response = web.GetResponse())
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        Debug.Assert(stream != null, "stream != null");
+                        using (var reader = new StreamReader(stream))
+                            strResponse = reader.ReadToEnd();
+                    }
+                }
+
+                var parseObject = JObject.Parse(strResponse);
+                result = Points(parseObject["routes"][0]["overview_polyline"]["points"].ToString(), 1e5);
+            }
+            catch (WebException e)
+            {
+                session.EventDispatcher.Send(new WarnEvent
+                {
+                    Message = $"Web Exception: {e.Message}"
+                });
+            }
+            catch (NullReferenceException e)
+            {
+                session.EventDispatcher.Send(new WarnEvent
+                {
+                    Message = $"Routing Error: {e.Message}"
+                });
+            }
+
+            return result;
+        }
+
+        public static List<List<double>> Points(string overview, double precision)
+        {
+            if (string.IsNullOrEmpty(overview))
+                throw new ArgumentNullException("Points");
+
+            var polyline = false;
+            int index = 0, lat = 0, lng = 0;
+            var polylineChars = overview.ToCharArray();
+            var result = new List<List<double>>();
+
+            while (index < polylineChars.Length)
+            {
+                int sum = 0, shifter = 0, nextBits;
+                var coordinates = new List<double>();
+
+                do
+                {
+                    nextBits = polylineChars[index++] - 63;
+                    sum |= (nextBits & 0x1f) << shifter;
+                    shifter += 5;
+                } while (nextBits >= 0x20 && index < polylineChars.Length);
+
+                if (index >= polylineChars.Length && (!polyline || nextBits >= 0x20))
+                    break;
+
+                if (!polyline)
+                    lat += (sum & 1) == 1 ? ~(sum >> 1) : sum >> 1;
+                else
+                {
+                    lng += (sum & 1) == 1 ? ~(sum >> 1) : sum >> 1;
+                    coordinates.Add(lng / precision);
+                    coordinates.Add(lat / precision);
+                    result.Add(coordinates);
+                }
+
+                polyline = !polyline;
+            }
+
+            return result;
+        }
+
+        public static event GetHumanizeRouteDelegate GetHumanizeRouteEvent;
+
+        protected virtual void OnGetHumanizeRouteEvent(List<GeoCoordinate> route)
+        {
+            GetHumanizeRouteEvent?.Invoke(route);
+        }
+        //end functions routes map
+    }
+}
